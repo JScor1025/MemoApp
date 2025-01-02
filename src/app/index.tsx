e935d65efdf780ebfbc41d92e5f4884690244ff4
joji <138176325+JScor1025@users.noreply.github.com>
2025-01-03 08:59:42 +0900

    Set up Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,48 +3,45 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
 
-            <View>
+            <View style={styles.item}>
                 <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年10月25日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
+                    <Text style={styles.itemTitle}>買い物リスト</Text>
+                    <Text style={styles.itemDate}>2024年10月25日 10:00</Text>
                 </View>
-
                 <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年10月25日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
+                    <Text>X</Text>
                 </View>
-
-                <View>
-                    <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2024年10月25日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.item}>
+                <View>
+                    <Text style={styles.itemTitle}>買い物リスト</Text>
+                    <Text style={styles.itemDate}>2024年10月25日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+
+            <View style={styles.item}>
+                <View>
+                    <Text style={styles.itemTitle}>買い物リスト</Text>
+                    <Text style={styles.itemDate}>2024年10月25日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+            </View>
+
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
         </View>
     )
@@ -53,8 +50,68 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#ffffff'
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end',
+    },
+    headerInner: {
+        alignItems: 'center'
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 19,
+        bottom: 16,
+        color: 'rgba(255,255,255,0.7)'
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    item: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+    itemTitle: {
+        fontSize: 16,
+        lineHeight: 32,
+    },
+    itemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#467FD3',
         justifyContent: 'center',
-        alignItems:'center'
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: {width: 0, height: 8},
+        elevation: 8
+    },
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 40
     }
 })
 
